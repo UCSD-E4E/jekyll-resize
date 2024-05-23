5a943e7a1bf9ee058458c6ff98e56b3bde4eab97
Sean Perry <sean.hyatt.perry@gmail.com>
2024-05-23 15:48:00 -0700

    Update jekyll-resize.rb

--- lib/jekyll-resize.rb
+++ lib/jekyll-resize.rb
@@ -35,6 +35,7 @@ module Jekyll
 
     # Determine whether the image needs to be written.
     def _must_create?(src_path, dest_path)
+      puts !File.exist?(dest_path), File.mtime(dest_path) <= File.mtime(src_path)
       !File.exist?(dest_path) || File.mtime(dest_path) <= File.mtime(src_path)
     end
 
